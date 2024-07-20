296a4367296fa56a9614d067ea66433447bf2c8c
liuyy337 <liuyy337@gmail.com>
2024-07-20 10:28:35 +0800

    first commit

---
+++ mv_aia.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mv *.94.image_lev1.fits ../94/
+mv *.131.image_lev1.fits ../131/
+mv *.171.image_lev1.fits ../171/
+mv *.193.image_lev1.fits ../193/
+mv *.211.image_lev1.fits ../211/
+mv *.304.image_lev1.fits ../304/
+mv *.335.image_lev1.fits ../335/
+mv *.1600.image_lev1.fits ../1600/
+mv *.1700.image_lev1.fits ../1700/
+
